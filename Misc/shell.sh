c3a7b2364d13f7e17e2595a02dd898260d247951
Quinn Favo <qfavo@abacusgroupllc.com>
2023-05-15 18:14:26 -0400

    sync

--- Misc/shell.sh
+++ Misc/shell.sh
@@ -5,3 +5,9 @@ ssh -i ssh-key-2023-05-15.key ubuntu@redacted
 ssh -i ssh-key-2023-05-15secondary.key ubuntu@redacted
 
 #Need to add allow rule in "Security list" for relevant Instances in the Oracle cloud
+
+#Secondary jenkins node install
+
+#https://hostadvice.com/how-to/web-hosting/ubuntu/how-to-setup-jenkins-master-and-slave-on-ubuntu-18-04-lts/
+
+ssh-copy-id Jenkins@10.0.0.138
