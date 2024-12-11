799b48ef7df0e74a7b6d4d7ba1dc131729343b39
Quinn Favo <flyingpork15@gmail.com>
2024-12-11 09:14:23 -0500

    push

---
+++ Oracle/Connect.sh
@@ -0,0 +1,1 @@
+ssh ubuntu@129.158.60.17 -i /Users/quinnfavo/Desktop/ssh-key-2024-12-09.key -p 22
--- Plex/Plex.sh
+++ Plex/Plex.sh
@@ -1,3 +1,14 @@
 #fstab
 
 UUID=B2B0-1E37  /mnt/Cruzer4GB  vfat  defaults,nofail  0  0 
+
+
+#
+#Add perms
+sudo chown root:sftpusers /mnt/Cruzer4GB/YouTube
+sudo chmod 775 /mnt/Cruzer4GB/YouTube
+
+#remove special chars before moving
+sudo cp * /mnt/Cruzer4GB/YouTube --no-preserve=ownership
+
+cp "Cantico_nuevo_PRAISE_Trato_de_recordar_su_amor_sin_igual_Dios_Madre.m4a" /mnt/cruzer4gb/youtube
