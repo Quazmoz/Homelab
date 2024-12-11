7d906040eab517fedd0893148b4645a11bab94fa
Quinn Favo <flyingpork15@gmail.com>
2024-12-11 09:57:37 -0500

    push

--- Oracle/Connect.sh
+++ Oracle/Connect.sh
@@ -1,1 +1,3 @@
 ssh ubuntu@129.158.60.17 -i /Users/quinnfavo/Desktop/ssh-key-2024-12-09.key -p 22
+#Download file for VPN
+scp -i /Users/quinnfavo/Desktop/ssh-key-2024-12-09.key ubuntu@129.158.60.17:/home/ubuntu/qfavo.conf  /Users/quinnfavo/Desktop
